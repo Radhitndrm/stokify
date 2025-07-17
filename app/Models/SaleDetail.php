d5fe10fd2e7fda9dae78db6301585649a0661768
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-17 23:55:58 +0800

    edit sale

--- app/Models/SaleDetail.php
+++ app/Models/SaleDetail.php
@@ -8,4 +8,14 @@ class SaleDetail extends Model
 {
     //
     protected $guarded = ['id'];
+
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class);
+    }
+
+    public function product_unit()
+    {
+        return $this->belongsTo(ProductUnit::class);
+    }
 }

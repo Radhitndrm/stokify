211c8164e07e7035ebe4be83d5e53dbb5a3417c7
Radhitndrm <radhit.andromeda@gmail.com>
2025-07-29 22:13:29 +0800

    Sidebar

---
+++ resources/js/components/sidebar-menu/side-stat.tsx
@@ -0,0 +1,28 @@
+import { ChartNoAxesCombined } from "lucide-react";
+import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "../ui/sidebar";
+import { Link } from "@inertiajs/react";
+
+type sideStatProps = {
+    url: string
+    setOpenMobile: (open: boolean) => void
+}
+
+export function SideStat({ url, setOpenMobile }: sideStatProps) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Stats</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton asChild tooltip={"Dashboard"}>
+                            <Link href='' onClick={() => setOpenMobile(false)}>
+                                <ChartNoAxesCombined />
+                                <span>Dashboard</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}

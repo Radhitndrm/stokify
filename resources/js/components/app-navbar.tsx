1225999bd03218d8273bb889fe1237b6483b8d0b
Radhitndrm <radhit.andromeda@gmail.com>
2025-08-01 23:24:02 +0800

    navbar done

---
+++ resources/js/components/app-navbar.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import { SidebarTrigger } from "./ui/sidebar";
+import { Separator } from "./ui/separator";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "./ui/breadcrumb";
+import { usePage, Link } from "@inertiajs/react";
+export function AppNavbar() {
+    const { url } = usePage();
+
+    const BreadCrumbItems = [
+        {
+            name: 'Stats',
+            isActive: url === '',
+            subItems: [
+                {
+                    name: 'Dashboard',
+                    isActive: url === '',
+                    href: ''
+                }
+            ],
+        },
+        {
+            name: 'Master Data',
+            isActive: '',
+            subItems: [
+                {
+                    name: 'Satuan',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Kategori',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Supplier',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Produk',
+                    isActive: '',
+                    href: '',
+                }
+            ],
+        },
+        {
+            name: 'Transaksi',
+            isActive: '',
+            subItems: [
+                {
+                    name: 'Pembelian',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Penjualan',
+                    isActive: '',
+                    href: '',
+                },
+            ],
+        },
+        {
+            name: 'Manajemen Stok',
+            isActive: '',
+            subItems: [
+                {
+                    name: 'Hak Akses',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Penerimaan Pembelian',
+                    isActive: '',
+                    href: '',
+                },
+            ],
+        },
+        {
+            name: 'Manajemen Pengguna',
+            isActive: '',
+            subItems: [
+                {
+                    name: 'Hak Akses',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Akses Group',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Pengguna',
+                    isActive: '',
+                    href: '',
+                },
+            ],
+        },
+        {
+            name: 'Laporan',
+            isActive: '',
+            subItems: [
+                {
+                    name: 'Kartu Stok',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Sisa Stok',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Pembelian',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Pembelian Belum Diterima',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Penjualan',
+                    isActive: '',
+                    href: '',
+                },
+                {
+                    name: 'Produk Terlaris',
+                    isActive: '',
+                    href: '',
+                },
+            ],
+        }
+    ];
+    return (
+        <div className="flex items-center gap-2 px-4 w-full">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+                <BreadcrumbList>
+                    {BreadCrumbItems.map((item, index) => (
+                        item.isActive && item.subItems.find(subItem => subItem.isActive) && (
+                            <React.Fragment key={index}>
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink asChild>
+                                        <Link href="#">{item.name}</Link>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                                {item.subItems.filter(subItem => subItem.isActive)
+                                    .map((subItem, subIndex) => (
+                                        <BreadcrumbItem key={subIndex}>
+                                            <BreadcrumbLink asChild>
+                                                <Link href={subItem.href}>{subItem.name}</Link>
+                                            </BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                    ))
+                                }
+                            </React.Fragment>
+                        )
+                    ))}
+                </BreadcrumbList>
+            </Breadcrumb>
+        </div>
+    )
+}
